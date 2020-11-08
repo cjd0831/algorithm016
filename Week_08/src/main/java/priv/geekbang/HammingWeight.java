88724c91373137a02c155e737ee4ad52614bf6cc
jingdong.cao <jingdong.cao@tendcloud.com>
2020-11-08 21:40:38 +0800

    complete week8 homework

---
+++ Week_08/src/main/java/priv/geekbang/HammingWeight.java
@@ -0,0 +1,16 @@
+package priv.geekbang;
+
+/**
+ * 位1的个数
+ */
+public class HammingWeight {
+
+  public int hammingWeight(int n) {
+    int sum = 0;
+    while (n != 0) {
+      sum++;
+      n &= (n - 1);
+    }
+    return sum;
+  }
+}
